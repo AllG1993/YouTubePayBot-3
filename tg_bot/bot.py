e338d5beeec7a6293c972e91f47ba9059aae118e
Aleksey G <alekseygalkovich@gmail.com>
2022-02-20 15:15:13 +0200

    try docker 4

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -7,7 +7,7 @@ from tg_bot.handlers.commands import im, start_help, paid
 from filters.is_trusted_user import IsTrustedUser
 from tg_bot.loader import dp
 
-sys.path.insert(0, os.path.abspath(''))
+sys.path.insert(0, os.path.abspath('..'))
 dp.filters_factory.bind(IsTrustedUser)
 
 dp.register_message_handler(start_help, commands=['start', 'help'], is_trusted_user=True)
--- tg_bot/tst.py
+++ tg_bot/tst.py
@@ -1,7 +1,8 @@
+import os
 import sys
 from pathlib import Path
 import google_sheets_api
 
 p = Path(Path.cwd().parent, 'ytpb-3-test-creds.json')
 
-print(p)
+print(os.path.abspath('..'))
